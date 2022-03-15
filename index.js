0b4bdcf316a9c9b10b6150031723d7757ad73f06
BetoNajera <ing.betonajera@gmail.com>
2022-03-14 21:15:13 -0600

    :sparkles: feat: Add RC4 algorithm
    Add KSA and RPGA methods

---
+++ index.js
@@ -0,0 +1,51 @@
+const { stdin: input } = require('process')
+
+input.resume()
+
+input.on('data', (data) => {
+	const srtingData = data.toString('utf-8')
+	const dataArray = srtingData.split(/\n/)
+	const Ksa = KSA(dataArray[0])
+	RPGA(dataArray[1], Ksa)
+})
+
+const keylength = (key) => {
+	return Buffer.from(key).length
+}
+
+const swap = (array, i, j) => {
+	const aux = array[i]
+	array[i] = array[j]
+	array[j] = aux
+
+	return array
+}
+
+const KSA = (key) => {
+	let S = Array.from(Array(256).keys())
+
+	let j = 0
+
+	for (i in S) {
+		j = (j + S[i] + key.charCodeAt(i % keylength(key))) % 256
+		S = swap(S, i, j)
+	}
+
+	return S
+}
+
+const RPGA = (message, S) => {
+	let i = 0
+	let j = 0
+	const result = []
+
+	for (l in message) {
+		i = (i + 1) % 256
+		j = (j + S[i]) % 256
+		S = swap(S, i, j)
+		const K = S[(S[i] + S[j]) % 256]
+		result.push((K ^ message.charCodeAt(l)).toString(16))
+	}
+
+	console.log(result.join('').toUpperCase())
+}
